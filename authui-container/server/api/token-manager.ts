25798c74d60f99f39775139a9b57e77900f4babe
Ido Flatow <ido.flatow@gmail.com>
2023-06-05 19:04:02 +0300

    Refactor token manager

--- authui-container/server/api/token-manager.ts
+++ authui-container/server/api/token-manager.ts
@@ -13,7 +13,6 @@
  */
 
 import {HttpServerRequestHandler} from '../../server/utils/http-server-request-handler';
-import requestPromise = require('request-promise');
 
 /** Interface defining a Google OAuth access token. */
 export interface GoogleOAuthAccessToken {
@@ -31,20 +30,9 @@ export interface Credential {
   getAccessToken(): Promise<GoogleOAuthAccessToken>;
 }
 
-export interface GcipToken {
-  verifyAccessToken(): Promise<boolean>;
-  getNameIdentifier(token: any): string;
-}
-
 /** Metadata server access token endpoint. */
 const METADATA_SERVER_ACCESS_TOKEN_URL =
-    'https://forward-proxy-usp2kumbna-zf.a.run.app/computeMetadata/v1/instance/service-accounts/default/token';
-
-const REFRESH_TOKEN_URL =
-    'https://securetoken.googleapis.com/v1/token?key=';
-
-const CLIENT_CERT_URL = 
-    'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com';
+    'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token';
 
 /** The default OAuth scope to include in the access token. */
 const DEFAULT_OAUTH_SCOPE = 'https://www.googleapis.com/auth/cloud-platform';
@@ -108,84 +96,5 @@ export class TokenManager implements Credential {
   /** Reset cached access tokens. */
   reset() {
     this.accessToken = null;
-  }
-}
-
-export class GcipTokenManager implements GcipToken {
-  private readonly googleSigningCertRetriever: HttpServerRequestHandler;
-  private expirationTime: number;
-  private clientCerts: any;
-  private accessToken: string | null;
-  private refreshToken: string | null;
-  private apiKey: string;
-
-  /**
-   */
-  constructor(accessToken:string, refreshToken: string, apiKey: string) {
-    this.googleSigningCertRetriever = new HttpServerRequestHandler({
-      method: 'GET',
-      url: CLIENT_CERT_URL,
-      timeout: TIMEOUT_DURATION,
-    });
-   
-    this.accessToken = accessToken;
-    this.refreshToken = refreshToken;
-    this.apiKey = apiKey;
-  }
-
-  private async RefreshToken() {
-
-  }
-
-  getNameIdentifier(token: any): string {
-    return token.email || token.firebase.identities[token.firebase.sign_in_provider][0];
-  }
-
-  async verifyAccessToken(allowRefresh:boolean=true): Promise<any> {
-    if (!this.clientCerts) {
-      const httpResponse = await this.googleSigningCertRetriever.send(null, "Unable to retrieve Google's signing certificate");
-      
-      if (httpResponse.statusCode === 200 && httpResponse.body) {
-          this.clientCerts = httpResponse.body;
-      } else {
-          throw new Error("Unable to retrieve Google's signing certificate");
-      }
-    }
-
-    var jwt = require('jsonwebtoken');
-    const decodedToken = jwt.decode(this.accessToken, {complete: true});
-    const kid = decodedToken.header.kid;
-    if (this.clientCerts[kid]) {
-      try {
-        const decoded = jwt.verify(this.accessToken, this.clientCerts[kid]);
-        return decoded;
-      } catch(err) {
-          if (err.name == 'TokenExpiredError' && allowRefresh) {
-            // Refresh the token and retry
-            const options = { 
-              method: 'POST',
-              uri: REFRESH_TOKEN_URL+this.apiKey,
-              form: {
-                grant_type: 'refresh_token',
-                refresh_token: this.refreshToken,
-              },
-            }
-
-            try {
-              const result = await requestPromise(options);
-              this.accessToken = JSON.parse(result).access_token;
-            } catch (err) {
-              throw new Error(`Failed refreshing access token: ${err.message}`);
-            }
-
-            // Retry validation with a new access token (no refresh this time!)
-            return await this.verifyAccessToken(false);
-          }
-          throw new Error(`Failed verifying JWT of user: ${err.message}`);
-      }
-    }
-    else {
-      throw new Error("JWT signing cert not found");
-    }
   }
 }
